2ccc1320e21dedc424f680394b298ab874cd123f
DariaVor <daria_voronina@list.ru>
2024-12-13 19:29:23 +0300

    add ticket type

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+
+type Ticket = {
+  id: number;
+  origin: string;
+  origin_name: string;
+  destination: string;
+  destination_name: string;
+  departure_date: string;
+  departure_time: string;
+  arrival_date: string;
+  arrival_time: string;
+  carrier: string;
+  stops: number;
+  price: number;
+};
+
+const CURRENCY_RATES = { USD: 0.013, EUR: 0.012, RUB: 1 };
 
 function App() {
-  const [tickets, setTickets] = useState([]);
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+  const [currency, setCurrency] = useState<"RUB" | "USD" | "EUR">("RUB");
 
   useEffect(() => {
     fetch("/tickets.json")
@@ -10,24 +27,30 @@ function App() {
       .then((data) => setTickets(data.tickets));
   }, []);
 
-  return <>
-  <div>
-    <h1>Билеты</h1>
-    {tickets.map((ticket) => (
-      <div key={ticket.id}>
-        <img src={`/logos/${ticket.carrier}.svg`}/>
-        <button>Купить за {ticket.price}₽</button>
-        <p>{ticket.departure_time}</p>
-        <p>{ticket.origin}, {ticket.origin_name}</p>
-        <p>{ticket.departure_date}</p>
-        <p>{ticket.stops} пересадка</p> 
-        <p>{ticket.arrival_time}</p>
-        <p>{ticket.destination_name}, {ticket.destination}</p>
-        <p>{ticket.departure_date}</p>
+  return (
+    <>
+      <div>
+        <h1>Билеты</h1>
+        {tickets.map((ticket) => (
+          <div key={ticket.id}>
+            <img src={`/logos/${ticket.carrier}.svg`} />
+            <button>Купить за {ticket.price}₽</button>
+            <p>{ticket.departure_time}</p>
+            <p>
+              {ticket.origin}, {ticket.origin_name}
+            </p>
+            <p>{ticket.departure_date}</p>
+            <p>{ticket.stops} пересадка</p>
+            <p>{ticket.arrival_time}</p>
+            <p>
+              {ticket.destination_name}, {ticket.destination}
+            </p>
+            <p>{ticket.departure_date}</p>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-  </>;
+    </>
+  );
 }
 
 export default App;
