1fc854bed1977feb31a8bb02c95c1f782d0b40cd
DariaVor <daria_voronina@list.ru>
2024-12-13 16:43:24 +0300

    get json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tickets, setTickets] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  useEffect(() => {
+    fetch("/tickets.json")
+      .then((response) => response.json())
+      .then((data) => setTickets(data.tickets));
+  }, []);
+
+  return <>
+  <div>
+    <h1>Билеты</h1>
+    {tickets.map((ticket) => (
+      <div key={ticket.id}>
+        <img src={`/logos/${ticket.carrier}.svg`}/>
+        <button>Купить за {ticket.price}₽</button>
+        <p>{ticket.departure_time}</p>
+        <p>{ticket.origin}, {ticket.origin_name}</p>
+        <p>{ticket.departure_date}</p>
+        <p>{ticket.stops} пересадка</p> 
+        <p>{ticket.arrival_time}</p>
+        <p>{ticket.destination_name}, {ticket.destination}</p>
+        <p>{ticket.departure_date}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    ))}
+  </div>
+  </>;
 }
 
-export default App
+export default App;
